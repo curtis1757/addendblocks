706ff8ae8849801e89073ffad0d01755df31bfc8
C Case <curtis1757@gmail.com>
2022-03-06 01:42:48 -0800

    code cleanup

--- addendblocks/addendblocks.py
+++ addendblocks/addendblocks.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 MIT License
 
@@ -27,12 +28,16 @@ This simple python program adds a comment line to Python source code at the end
 of each indented code block 
 """
 
+__version__ = '1.0'
+__author__ = 'Curtis Case'
+
 import os
 import sys
 import datetime
 import pathlib
 import argparse
 import glob
+
 from tokenize import generate_tokens, detect_encoding, TokenError
 from token import *
 
@@ -48,7 +53,7 @@ indent_key_words = {'if': ('elif', 'else'),
 #-------------------------------------------------------------------------------------------------------------
 def untokenize(tokens):
     """
-    Rebuild the python code source file from the tokdens list and add in
+    Add to the python source file tokens list 
     properly indented comments at the end of each indented block.
     """
     tokens_out = []
@@ -82,11 +87,12 @@ def untokenize(tokens):
                     #--Do we need an end block for this?
                     if (token in indent_key_words[indents[-1][0]] and start[1] == indents[-1][1]) or start[1] > indents[-1][1]: indents[-1][2] = True
 
-                    #--If our last indent was for a keyword (like 'if) with associated keywords (like 'else') and the current token/kew word is not one these associated keywords
-                    #-- and we are at the same indent level  OR is the current indent level is less than the nlast indent level... then we need add min one or more 'end block comments'
+                    #--If our last indent was for a keyword (like 'if) with associated keywords (like 'else') and the current token/kew word is not
+                    #--one these associated keywords and we are at the same indent level  OR is the current indent level is less than the last indent level...
+                    #--then we need add one or more 'end block comments'
                     if ((token not in indent_key_words[indents[-1][0]] and start[1] == indents[-1][1]) or start[1] < indents[-1][1]):
 
-                        #--determine where to add then e d block comment; should be after last line of code, before comments or blank lines
+                        #--determine where to add the end block comment; should be after last line of code, before comments or blank lines
                         ins_at = last_cmt_nl if last_cmt_nl != -1 else len(tokens_out)
 
                         #--Now add in the 'end block comments' until the indent level is less than the curremnt indent level 
@@ -99,7 +105,8 @@ def untokenize(tokens):
 
                             indents.pop()
 
-                            #--if we have something like 'if a == b: print(a)' ie all on one line with no folowing 'else' or 'elif', then we won't put the end blk comment in
+                            #--if we have something like 'if a == b: print(a)' ie all on one line with no folowing 'else' or 'elif',
+                            #--then we won't put the end blk comment in
                             if need_endblk:
                                 tokens_out.insert(ins_at, f"{' ' * indent_size}{end_blk_cmts_dict[dedent_name]}{newline_end}")
                                 ins_at += 1
@@ -143,6 +150,8 @@ def untokenize(tokens):
             continue
             
         elif tok_type in (NEWLINE, NL):
+
+            #--Remember then end of line character(s) for adding at end of end block comments, etc.
             if newline_end == None: newline_end = token
 
             if token not in ("\n", "\r\n", "\r"):
@@ -193,6 +202,7 @@ def untokenize(tokens):
     #end for
 
     #--Do dedenting if needed at end of file
+    #--determine where to add the end block comment; should be after last line of code, before comments or blank lines
     ins_at = last_cmt_nl if last_cmt_nl != -1 else len(tokens_out)
     for _ in range(len(indents),0,-1): 
         dedent_name, indent_size, need_endblk = indents.pop()
@@ -208,9 +218,9 @@ def untokenize(tokens):
 #end def
 
 #-------------------------------------------------------------------------------------------------------------
-def remove_end_blocks(source_lines):
+def remove_end_blocks(source):
     stripped = []
-    for line in source_lines: #.splitlines():
+    for line in source:
         ls = line.strip()
         if ls.replace('# ','#') not in end_blk_cmts_dict.values() and ls not in end_blk_cmts_dict.values(): stripped.append(line)
     #end for
@@ -232,7 +242,7 @@ def make_tokens(source_lines):
 #end def
 
 #-------------------------------------------------------------------------------------------------------------
-def check_to_overide_end_blk(kw, str):
+def check_to_overide_end_blk_cmt(kw, str):
     global end_blk_cmts_dict
     if str != '':
         sout = []
@@ -316,46 +326,46 @@ def setup_end_blocks(options):
     if not options.do_case_blk_end: end_blk_cmts_dict['case'] = f"#{options.block_end_prefix}case{ options.block_end_suffix}"
 
     #--Optionally override individual end block comments for specific key words 
-    check_to_overide_end_blk('if', options.block_end_if)
-    check_to_overide_end_blk('try', options.block_end_try)
-    check_to_overide_end_blk('for', options.block_end_for)
-    check_to_overide_end_blk('while', options.block_end_while)
-    check_to_overide_end_blk('with', options.block_end_with)
-    check_to_overide_end_blk('def', options.block_end_def)
-    check_to_overide_end_blk('class', options.block_end_class)
-    check_to_overide_end_blk('match', options.block_end_match)
-    check_to_overide_end_blk('case', options.block_end_case)
+    check_to_overide_end_blk_cmt('if', options.block_end_if)
+    check_to_overide_end_blk_cmt('try', options.block_end_try)
+    check_to_overide_end_blk_cmt('for', options.block_end_for)
+    check_to_overide_end_blk_cmt('while', options.block_end_while)
+    check_to_overide_end_blk_cmt('with', options.block_end_with)
+    check_to_overide_end_blk_cmt('def', options.block_end_def)
+    check_to_overide_end_blk_cmt('class', options.block_end_class)
+    check_to_overide_end_blk_cmt('match', options.block_end_match)
+    check_to_overide_end_blk_cmt('case', options.block_end_case)
 #end def
 
 #-------------------------------------------------------------------------------------------------------------
-def add_end_blocks(source_lines, filename, encoding):
+def add_end_blk_cmts(source_lines, fn, encoding):
 
     try:
+        #--First tokenize input spython source code
         tokens = make_tokens(source_lines)
         
         #--optionally save the tokens to a file
         if tokensave: 
-            with open(f"{filename}.tokens.txt", 'w', encoding=encoding) as fout: fout.writelines(f"{token}\n" for token in tokens)
+            with open(f"{fn}.tokens.txt", 'w', encoding=encoding) as fout: fout.writelines(f"{token}\n" for token in tokens)
         #end if
 
-        #--Regenerate the source code file with previously set options and write to file
-
+        #--Add end block comments to tokens list, using previously set options
         return untokenize(tokens)
 
     except IndentationError as err:
-        sys.stderr.write(f"IndentationError exception in {err.args[1][0]} while processing file:\n{filename}\nat line:{err.args[1][1]}, column:{err.args[1][2]} error: {err.args[0]}in line:\n{err.args[1][3]}")
+        sys.stderr.write(f"IndentationError exception in {err.args[1][0]} while processing file:\n{fn}\nat line:{err.args[1][1]}, column:{err.args[1][2]} error: {err.args[0]}in line:\n{err.args[1][3]}")
         return ''
 
     except TokenError as err:
-        sys.stderr.write(f"{filename}: at line:{err.args[1][0]}, column:{err.args[1][1]} error: {err.args[0]}\n")
+        sys.stderr.write(f"{fn}: at line:{err.args[1][0]}, column:{err.args[1][1]} error: {err.args[0]}\n")
         return ''
 
     except SyntaxError as err:
-        sys.stderr.write(f"{filename}: at line:{err.args[1][1]}, column:{err.args[1][2]} error: {err.args[0]}\n")
+        sys.stderr.write(f"{fn}: at line:{err.args[1][1]}, column:{err.args[1][2]} error: {err.args[0]}\n")
         return ''
 
     except ValueError as err:
-        sys.stderr.write(f"stderr> {filename}: at line:{err.args[1][0]}, column:{err.args[1][1]} error: {err.args[0]}\n")
+        sys.stderr.write(f"stderr> {fn}: at line:{err.args[1][0]}, column:{err.args[1][1]} error: {err.args[0]}\n")
         sys.stderr.write(f"{err}\n")
         raise
     #end try
@@ -377,7 +387,7 @@ def get_encoding(filename):
 #-------------------------------------------------------------------------------------------------------------
 def main():
     
-    #--With options and file name form command line, set up end block comment parameters
+    #--With options and file name from command line, set up end block comment options
     setup_end_blocks(get_options_from_cmd_line())
 
     try:
@@ -385,16 +395,20 @@ def main():
         file_list = [fn for fn in glob.glob(filename, recursive=recursive) if pathlib.Path(fn).suffix.lower() == '.py' and os.path.isfile(fn)]
 
         for fn in file_list:
+
+            #--Determine file encoding
             encoding = get_encoding(fn)
 
-            #--read the spython source file
+            #--Read the python source file
             with open(fn, mode='rt', encoding=encoding, newline='') as f: source = f.readlines()
 
-            #--remove any old end block comments (Note: only removes comments in the same format that is currently selected)
+            #--Remove any old end block comments (Note: only removes comments in the same format that is currently selected)
             source = remove_end_blocks(source)
-
-            #--Now add the end block commants
-            if not onlyremove: source = add_end_blocks(source, fn, encoding)
+            
+            if not onlyremove:
+                #--Now add the end block comments
+                source = add_end_blk_cmts(source, fn, encoding)
+            #end if
 
             #--Save the new Python source file (replaces original)
             with open(fn, 'w', encoding=encoding, newline='') as fout: fout.write("".join(source))
